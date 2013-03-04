0f0c9e590d3038a63a2557658caa91f0b16f11c3
Matthew Somerville <matthew-github@dracos.co.uk>
2013-03-04 13:53:08 +0000

    Add list of instances on front page.

--- instances/models.py
+++ instances/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 from .fields import DNSLabelField
 
 class InstanceManager(models.Manager):
@@ -7,6 +9,15 @@ class InstanceManager(models.Manager):
 
 class Instance(models.Model):
     label = DNSLabelField( db_index=True, unique=True )
+
+    def __unicode__(self):
+        return u'Instance %s' % self.label
+
+    def get_absolute_url(self):
+        url = 'http://%s.%s' % (self.label, getattr(settings, 'BASE_HOST', '127.0.0.1.xip.io'))
+        if getattr(settings, 'BASE_PORT', None):
+            url += ':' + settings.BASE_PORT
+        return url
 
 class InstanceMixin(models.Model):
     instance = models.ForeignKey(Instance)
