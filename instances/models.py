3ce3f64d500c63a49fa01645d55c6c74339d60ec
Duncan Parkes <duncan@mysociety.org>
2014-06-24 22:39:36 +0100

    Placate flake8 on instances/models.py

--- instances/models.py
+++ instances/models.py
@@ -4,15 +4,17 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from .fields import DNSLabelField
 
+
 class InstanceManager(models.Manager):
     def for_instance(self, instance):
         return self.get_query_set().filter(instance=instance)
 
+
 @python_2_unicode_compatible
 class Instance(models.Model):
-    label = DNSLabelField( db_index=True, unique=True )
-    title = models.CharField( max_length=100 )
-    description = models.TextField( blank=True )
+    label = DNSLabelField(db_index=True, unique=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
     users = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         related_name='instances',
@@ -29,10 +31,14 @@ class Instance(models.Model):
         return u'Instance %s' % self.label
 
     def get_absolute_url(self):
-        url = 'http://%s.%s' % (self.label, getattr(settings, 'BASE_HOST', '127.0.0.1.xip.io'))
+        url = 'http://%s.%s' % (
+            self.label,
+            getattr(settings, 'BASE_HOST', '127.0.0.1.xip.io'),
+            )
         if getattr(settings, 'BASE_PORT', None):
             url += ':%s' % settings.BASE_PORT
         return url
+
 
 class InstanceMixin(models.Model):
     instance = models.ForeignKey(Instance)
