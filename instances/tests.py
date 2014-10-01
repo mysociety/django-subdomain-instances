d9246f8aef8676bfde98b122c3473844d6f349a5
Duncan Parkes <duncan@mysociety.org>
2014-10-01 17:23:33 +0100

    Placate flake8.
    Well mostly anyway - I thing the 80 character line looks worse split.

--- instances/tests.py
+++ instances/tests.py
@@ -8,15 +8,17 @@ from .models import Instance
 
 FAKE_URL = 'testing.example.org:8000'
 
+
 class InstanceClient(Client):
     def __init__(self, enforce_csrf_checks=False, **defaults):
         defaults.setdefault('HTTP_HOST', FAKE_URL)
         super(InstanceClient, self).__init__(
             enforce_csrf_checks=enforce_csrf_checks, **defaults)
 
+
 @override_settings(
     BASE_HOST='example.org',
-    PASSWORD_HASHERS = ( 'django.contrib.auth.hashers.MD5PasswordHasher', ),
+    PASSWORD_HASHERS=('django.contrib.auth.hashers.MD5PasswordHasher',),
 )
 class InstanceTestCase(TestCase):
     client_class = InstanceClient
@@ -27,7 +29,8 @@ class InstanceTestCase(TestCase):
 
     def setUp(self):
         self.instance = Instance.objects.create(**self.default_instance_options)
-        user = User.objects.create_user(username='admin', email='admin@example.org', password='admin')
+        user = User.objects.create_user(
+            username='admin', email='admin@example.org', password='admin')
         user.instances.add(self.instance)
         self.client.login(username='admin', password='admin')
 
@@ -35,7 +38,8 @@ class InstanceTestCase(TestCase):
         kwargs['host'] = FAKE_URL
         return super(InstanceTestCase, self).assertRedirects(*args, **kwargs)
 
-@override_settings( SESSION_COOKIE_DOMAIN='127.0.0.1.xip.io' )
+
+@override_settings(SESSION_COOKIE_DOMAIN='127.0.0.1.xip.io')
 class InstanceLiveServerTestCase(LiveServerTestCase):
     # Override in a subclass if you need to change what the default instance
     # created looks like.
@@ -47,12 +51,14 @@ class InstanceLiveServerTestCase(LiveServerTestCase):
             username='admin', email='admin@example.org', password='admin')
         user.instances.add(self.instance)
 
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/?next=/'))
+        self.selenium.get(
+            '%s%s' % (self.live_server_url, '/accounts/login/?next=/'))
         username_input = self.selenium.find_element_by_name("username")
         username_input.send_keys('admin')
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys('admin')
         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+
 
 # ---
 
@@ -62,7 +68,9 @@ class SimpleTest(TestCase):
         self.assertEqual(i.label, 'hello')
 
     def test_bad_label(self):
-        self.assertRaises(ValidationError, lambda: Instance(label='Spaces are not allowed'))
-        self.assertRaises(ValidationError, lambda: Instance(label='Nor-a-symbol-such-as-^'))
-        self.assertRaises(ValidationError, lambda: Instance(label="Nor-can-you-end-with--"))
-
+        self.assertRaises(
+            ValidationError, lambda: Instance(label='Spaces are not allowed'))
+        self.assertRaises(
+            ValidationError, lambda: Instance(label='Nor-a-symbol-such-as-^'))
+        self.assertRaises(
+            ValidationError, lambda: Instance(label="Nor-can-you-end-with--"))
