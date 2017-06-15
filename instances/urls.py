b9c1b355e2d72044d356bcaf23f113e08bed7fcd
Matthew Somerville <matthew-github@dracos.co.uk>
2017-06-15 21:04:19 +0100

    Remove patterns(), removed in Django 1.10.

--- instances/urls.py
+++ instances/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import ListView
 
 from instances.models import Instance
 
-urlpatterns = patterns(
-    '',
-    (r'^$', ListView.as_view(
+urlpatterns = [
+    url(r'^$', ListView.as_view(
         queryset=Instance.objects.all(),
         context_object_name='instances',
         template_name='instances/index.html',
     )),
-)
+]
 
 urlpatterns += staticfiles_urlpatterns()
