4e9187a70f8ac222c807889e39fe1c26dfe2b329
Matthew Somerville <matthew-github@dracos.co.uk>
2017-06-15 20:59:27 +0100

    Remove use of SubfieldBase.

--- instances/fields.py
+++ instances/fields.py
@@ -3,20 +3,36 @@ import re
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy
-from django.utils.six import with_metaclass
 
 label_re = re.compile(r'(?i)^[a-z0-9][a-z0-9-]*[a-z0-9]$')
 validate_label = RegexValidator(
     label_re, ugettext_lazy("Enter a valid instance label consisting of letters, numbers, or hyphens."), 'invalid')
 
 
-class DNSLabelField(with_metaclass(models.SubfieldBase, models.CharField)):
+class Creator(object):
+    def __init__(self, field):
+        self.field = field
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        return obj.__dict__[self.field.name]
+
+    def __set__(self, obj, value):
+        obj.__dict__[self.field.name] = self.field.to_python(value)
+
+
+class DNSLabelField(models.CharField):
     description = "A DNS label"
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = kwargs.get('max_length', 63)
         super(DNSLabelField, self).__init__(*args, **kwargs)
         self.validators.append(validate_label)
+
+    def contribute_to_class(self, cls, name):
+        super(DNSLabelField, self).contribute_to_class(cls, name)
+        setattr(cls, name, Creator(self))
 
     def to_python(self, value):
         value = super(DNSLabelField, self).to_python(value)
